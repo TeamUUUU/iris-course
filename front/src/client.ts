031a657405dbdb7f1d4004c85d28b3da9fe8167e
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2021-04-06 19:48:23 +0300

    add error message

--- front/src/client.ts
+++ front/src/client.ts
@@ -5,7 +5,7 @@ type Response<Result> = GeneralError | Result;
 
 export function getResult<Result>(response: Response<Result>): Result {
     if ((response as GeneralError).error) {
-        throw (response as GeneralError).error;
+        throw Error("Server returned error: " + (response as GeneralError).error);
     }
     return response as Result;
 };
