7c238219410810ce368883bbd9f8142c571e3a0d
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2021-04-07 21:01:27 +0300

    use different date formatting

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -211,6 +211,7 @@ const FormsPage = () => {
 	const user = useUser();
 	const [formList, setFormList] = React.useState<any>([]);
 	React.useEffect(() => {
+		const date_format = new Intl.DateTimeFormat(undefined, {year: 'numeric', month: 'short', day: 'numeric'});
 		const fetch = async () => {
 			const res = await api.getForms(user.id);
 			const forms = getResult(res).forms;
@@ -218,8 +219,8 @@ const FormsPage = () => {
 				id: f.id,
 				title: f.title,
 				description: f.subtitle,
-				dateFrom: (new Date(1000 * f.available_from)).toLocaleString(),
-				dateTo: (new Date(1000 * f.available_to)).toLocaleString(),
+				dateFrom: date_format.format(new Date(1000 * f.available_from)),
+				dateTo: date_format.format(new Date(1000 * f.available_to)),
 				link: f.link
 			}))
 			setFormList(formList);
